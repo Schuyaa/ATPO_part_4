eacb4d648d9bfb2e43f35cecb3dc70a7c37e67c0
schulya <scherbinintoscha@yandex.ru>
2025-05-20 21:04:21 +0500

    added comment about commit (check basket)

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -27,3 +27,11 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_correct_product_name_in_message(product_name)
     page.should_be_correct_product_price_in_message(product_price)
 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):#открытие корзины
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_text()
